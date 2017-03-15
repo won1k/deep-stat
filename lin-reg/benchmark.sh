3c3965d82361be5ca204b4db68a4c9c8ce6ec0c7
Won Lee <won_lee@g.harvard.edu>
2017-03-15 13:26:26 -0400

    benchmarks linreg

--- lin-reg/benchmark.sh
+++ lin-reg/benchmark.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -n 1                    # Number of cores
 #SBATCH -N 1                    # Ensure that all cores are on one machine
-#SBATCH -t 0-5:10              # Runtime in D-HH:MM
+#SBATCH -t 2-5:10              # Runtime in D-HH:MM
 #SBATCH -p stats         # Partition to submit to
-#SBATCH --mem=20000               # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=50000               # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o log/log_bench     # File to which STDOUT will be written
 #SBATCH -e err/err_bench      # File to which STDERR will be written
 
@@ -14,7 +14,7 @@ source activate deepstat
 
 #THEANO_FLAGS=device=gpu,floatX=float32 python my_keras_script.py
 
-for n in 1000 10000 10000
+for n in 10000 10000
 do
 	for p in 10 100 1000
 	do
